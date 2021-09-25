c3c666ef74eb0a398533459c7553cc92b42762b0
Dima-Shvets <ddimaschvets@gmail.com>
2021-09-25 17:06:22 +0300

    adds theme change feature

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import './sass/main.scss';
+import refs from './references/refs'
 import cards from './data/menu.json';
 import cardsTpl from './templates/card.hbs'
- 
-console.log(cardsTpl(cards));
+import { onInputChange, setSavedTheme } from './js/functions'
+
+const cardsMarkup = cardsTpl(cards);
+
+setSavedTheme()
+
+refs.themeSwitchBtn.addEventListener('change', onInputChange);
+
+refs.menu.innerHTML = cardsMarkup;
+
+
+
+
 
